cce4f63380ddfde45f429e9e46f662ab411a4dff
VulcanWhale <dasguptaanik462@gmail.com>
2025-08-11 12:39:59 +0530

    Refactor q9.py: Consolidated character validation and output logic

--- q9.py
+++ q9.py
@@ -1,31 +1,65 @@
+# # Wap to check whether the given character is digit or not.
+# 
+# n = input("Please enter a character: ")
+# 
+# c = int(input("Please enter a choice for which approach you wanna use: "))
+# 
+# if c == 1:
+#     if len(n) == 1:
+#         if n.isdigit():
+#             print(f"{n} is a digit")
+#         else:
+#             print(f"{n} is not a digit character")
+#     else:
+#         print("Please enter a single character only, multiple characters aren't allowed")
+# elif c == 2:
+#     if len(n) == 1:
+#         if '0' <= n <= '9':
+#             print(f"{n} is a digit")
+#         else:
+#             print(f"{n} is not a digit character")
+#     else:
+#         print("Please enter a single character only, multiple characters aren't allowed")
+# 
+# elif c == 3:
+#     if len(n) == 1:
+#         if n in '0123456789':
+#            print(f"{n} is a digit")
+#         else:
+#            print(f"{n} is not a digit character")
+#     else:
+#        print("Please enter a single character only, multiple characters aren't allowed") 
+
+# Refactored code starts here
+
 # Wap to check whether the given character is digit or not.
 
 n = input("Please enter a character: ")
 
-c = int(input("Please enter a choice for which approach you wanna use: "))
+if len(n) != 1:
+    print("Please enter a single character only, multiple characters aren't allowed")
+else:
+    try:
+        c = int(input("Please enter a choice for which approach you wanna use (1, 2, or 3): "))
+    except ValueError:
+        print("Invalid input for choice. Please enter a number.")
+        exit()
 
-if c == 1:
-    if len(n) == 1:
-        if n.isdigit():
-            print(f"{n} is a digit")
-        else:
-            print(f"{n} is not a digit character")
-    else:
-        print("Please enter a single character only, multiple characters aren't allowed")
-elif c == 2:
-    if len(n) == 1:
-        if '0' <= n <= '9':
-            print(f"{n} is a digit")
-        else:
-            print(f"{n} is not a digit character")
-    else:
-        print("Please enter a single character only, multiple characters aren't allowed")
+    is_digit = False # Initialize to False
 
-elif c == 3:
-    if len(n) == 1:
-        if n in '0123456789':
-           print(f"{n} is a digit")
-        else:
-            print(f"{n} is not a digit character")
+    if c == 1:
+        is_digit = n.isdigit()
+    elif c == 2:
+        is_digit = '0' <= n <= '9'
+    elif c == 3:
+        is_digit = n in '0123456789'
     else:
-        print("Please enter a single character only, multiple characters aren't allowed") 
+        print("Invalid choice. Please enter 1, 2, or 3.")
+        exit()
+
+    if is_digit:
+        print(f"{n} is a digit")
+    else:
+        print(f"{n} is not a digit character")
+
+# Refactored code ends here
