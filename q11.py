2605c52a7af8ea7f517a8fa3f5070a0ade3c7c90
VulcanWhale <dasguptaanik462@gmail.com>
2025-08-13 11:43:25 +0530

    refactor(q11): Add user choice for different approaches

--- q11.py
+++ q11.py
@@ -2,13 +2,25 @@
 
 n = eval(input("Please enter some value: "))
 
-if type(n) not in [bool,int,float,complex,str,tuple]:
-    print(f"'{n}' is a mutable data")
+c = input("Please enter a choice for which approach you wanna use (1, 2, or 3): ")
+
+print("-" * 20)
+
+if c == '1':
+    # Approach 1: Using if-else
+    if type(n) not in [bool, int, float, complex, str, tuple]:
+        print(f"'{n}' is a mutable data")
+    else:
+        print(f"'{n}' is an Immutable data")
+
+elif c == '2':
+    # Approach 2: Using ternary operator
+    print(True if type(n) in [list,set,dict] else False)
+
+elif c == '3':
+    # Approach 3: Using lambda function
+    is_mutable = lambda x: type(x) in [list, set, dict]
+    print(is_mutable(n))
+
 else:
-    print(f"'{n}' is an Immutable data")
-
-
-print(True if type(n) in [list,set,dict] else False)
-
-x = lambda n: type(n) in [list, set, dict]
-print(x(n))
+    print("Invalid choice. Please enter 1, 2, or 3.")
