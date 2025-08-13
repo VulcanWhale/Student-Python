13f161c73d55900f287c9a594534e85821c575c8
VulcanWhale <dasguptaanik462@gmail.com>
2025-08-13 11:45:41 +0530

    docs(q11): Add detailed explanations for each approach

--- q11.py
+++ q11.py
@@ -7,20 +7,29 @@ c = input("Please enter a choice for which approach you wanna use (1, 2, or 3): 
 print("-" * 20)
 
 if c == '1':
-    # Approach 1: Using if-else
+    print("Approach 1: Checking against a list of immutable types")
+    # This approach assumes anything not in the list is mutable.
+    # This may not be accurate for all types (e.g., frozenset).
     if type(n) not in [bool, int, float, complex, str, tuple]:
-        print(f"'{n}' is a mutable data")
+        print(f"The data '{n}' is considered MUTABLE.")
     else:
-        print(f"'{n}' is an Immutable data")
+        print(f"The data '{n}' is considered IMMUTABLE.")
 
 elif c == '2':
-    # Approach 2: Using ternary operator
-    print(True if type(n) in [list,set,dict] else False)
+    print("Approach 2: Ternary operator checking for common mutable types")
+    is_mutable = True if type(n) in [list, set, dict] else False
+    if is_mutable:
+        print(f"The data '{n}' is MUTABLE (list, set, or dict).")
+    else:
+        print(f"The data '{n}' is IMMUTABLE (or not a list, set, or dict).")
 
 elif c == '3':
-    # Approach 3: Using lambda function
-    is_mutable = lambda x: type(x) in [list, set, dict]
-    print(is_mutable(n))
+    print("Approach 3: Lambda function checking for common mutable types")
+    is_mutable_func = lambda x: type(x) in [list, set, dict]
+    if is_mutable_func(n):
+        print(f"The data '{n}' is MUTABLE (list, set, or dict).")
+    else:
+        print(f"The data '{n}' is IMMUTABLE (or not a list, set, or dict).")
 
 else:
     print("Invalid choice. Please enter 1, 2, or 3.")
