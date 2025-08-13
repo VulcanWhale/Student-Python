734996f68b5467005cb7360976197c1f5b294691
VulcanWhale <dasguptaanik462@gmail.com>
2025-08-13 11:28:07 +0530

    all approach written

--- q11.py
+++ q11.py
@@ -1,1 +1,14 @@
-# 
+# Wap to check whether the data is mutable or not.
+
+n = eval(input("Please enter some value: "))
+
+if type(n) not in [bool,int,float,complex,str,tuple]:
+    print(f"'{n}' is a mutable data")
+else:
+    print(f"'{n}' is an Immutable data")
+
+
+print(True if type(n) in [list,set,dict] else False)
+
+x = lambda n: type(n) in [list, set, dict]
+print(x(n))
