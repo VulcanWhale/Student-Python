c6d68a7354433144376009af5809b04b5e31a909
VulcanWhale <dasguptaanik462@gmail.com>
2025-09-06 13:16:21 +0530

    feat(q15): Add solution for question 15

---
+++ q15.py
@@ -0,0 +1,11 @@
+# Wap to check whether the number is even or odd.
+
+choice =  int(input("Please enter a choice (1 or 2): "))
+
+match choice:
+    case 1:
+        print(f"{a} is an even number" if (a := int(input("Please enter a number: "))) & 1 == 0 else f"{a} is an odd number")
+    case 2:
+        print(f"{a} is an even number" if (a := int(input("Please enter a number: ")))%2 != 1 else f"{a} is an odd number")
+    case _:
+        print("Invalid Choice!")
