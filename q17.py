a9933bbbe4593fcc612a1f1ae684e37b38c3ebb1
VulcanWhale <dasguptaanik462@gmail.com>
2025-09-09 12:49:59 +0530

    refactor: simplify boolean expressions in q17.py

--- q17.py
+++ q17.py
@@ -1,5 +1,14 @@
 # Wap to check whether 2 values are pointing to the same memory or not.
 import ast
-#print(True if (a := ast.literal_eval(input("Enter  some value: "))) is (b := ast.literal_eval(input("Enter  some value: "))) else False)
 
-print(True if id(a := ast.literal_eval(input("Enter  some value: "))) == id(b := ast.literal_eval(input("Enter  some value: "))) else False)
+c = int(input("please enter some choices(1 or 2): "))
+
+match c:
+    case 1:
+        print((a := ast.literal_eval(input("Enter  some value: "))) is (b := ast.literal_eval(input("Enter  some value: "))))
+        del a,b
+    case 2:
+        print(id(a := ast.literal_eval(input("Enter  some value: "))) == id(b := ast.literal_eval(input("Enter  some value: "))))
+        del a,b
+    case _:
+        print("invalid choice")
