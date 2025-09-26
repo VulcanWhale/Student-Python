d84d528d651e042e8d9f64eadb52fcaf8be524e9
VulcanWhale <dasguptaanik462@gmail.com>
2025-09-26 10:53:20 +0530

    deleted the golbal variables

--- q15.py
+++ q15.py
@@ -5,7 +5,10 @@ choice =  int(input("Please enter a choice (1 or 2): "))
 match choice:
     case 1:
         print(f"{a} is an even number" if (a := int(input("Please enter a number: "))) & 1 == 0 else f"{a} is an odd number")
+        a = None
+        print(a)
     case 2:
         print(f"{a} is an even number" if (a := int(input("Please enter a number: ")))%2 != 1 else f"{a} is an odd number")
+        del a
     case _:
         print("Invalid Choice!")
