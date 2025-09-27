afa3d1d0b5b37f023048e3e0fe006ac4a2da629d
VulcanWhale <dasguptaanik462@gmail.com>
2025-09-27 13:21:22 +0530

    docs(q26): add and correct comments

--- q26.py
+++ q26.py
@@ -2,10 +2,16 @@
 
 def check():
     while True:
+        # using infinite loop so that untill the user enters the correct input, it will continue to take the inputs
         try:
+             # taking the inputs and comparing the two values
              print("the numbers are equal" if (a:= int(input("Please enter an integer: ")))==(b := int(input("Please enter the 2nd integer value: "))) else f"{a} is greater than {b}" if a>b else f"{a} is less than {b}")
-             break
+
+             break      # coming out of the loop as my work is done
         except ValueError:
+            # handling the specific value error exception
             print("Please enter an integer only.....(12,23,-45,etc.)")
 
+
+# final working of the functional program
 check()
