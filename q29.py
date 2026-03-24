c9f21f2bf06a0c6a006149b658b39827fbe29996
VulcanWhale <dasguptaanik462@gmail.com>
2026-03-24 23:57:43 +0530

    feat(q29): refine login flow to ask only for password once username is correct

--- q29.py
+++ q29.py
@@ -2,15 +2,17 @@
 uname = "Anik"
 pas = "Anik@123"
 
-    
 while True:
-    if uname == (u := input("Please enter the username: ")):
-        p = input("Please enter the password: ")
-        if p == pas:
-            print("login successfull")
-            break
-        else:
-            print("please enter the correct password.")
+    if uname == input("Please enter the username: "):
+        # Nested loop: Once username is correct, only ask for the password
+        while True:
+            p = input("Please enter the password: ")
+            if p == pas:
+                print("login successful")
+                # Exit both loops
+                break 
+            else:
+                print("please enter the correct password.")
+        break # Exit the outer loop once the inner one breaks (after success)
     else:
         print("please enter the correct username.")
-
