15d6d44587de780d42f6b5e241847685316cbf86
VulcanWhale <dasguptaanik462@gmail.com>
2026-03-25 13:37:02 +0530

    feat(q30): implement basic middle-value check for string lists

--- q30.py
+++ q30.py
@@ -1,1 +1,12 @@
 # 30. Wap to print the middle value of a list only if it is string.
+import ast
+
+l = ast.literal_eval(input("please enter a list: "))
+
+if len(l)%2 == 0:
+    print("this list has no middle value")
+else:
+    if isinstance(l[len(l)//2],str):
+        print(f"the middle value is : {l[len(l)//2]}")
+    else:
+        print("middle value isn't a string")
